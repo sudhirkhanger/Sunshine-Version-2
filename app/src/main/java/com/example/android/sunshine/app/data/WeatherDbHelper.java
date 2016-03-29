022817078aa2056049a273a80f8ded0df6ac5202
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-03-29 23:51:46 +0530

    location db

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
@@ -70,6 +70,14 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                LocationEntry._ID + " INTEGER PRIMARY KEY," +
+
+                LocationEntry.COLUMN_CITY_NAME + " STRING NOT NULL, " +
+                LocationEntry.COLUMN_LOCATION_SETTING + " INTEGER NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LAT + "FLOAT NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LONG + "FLOAT NOT NULL);";
     }
 
     @Override
